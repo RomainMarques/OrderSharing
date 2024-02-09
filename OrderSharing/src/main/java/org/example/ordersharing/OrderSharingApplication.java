50e431d4a71e4917e9f3aad8c1b016df1410de09
RomainMarques <77971683+RomainMarques@users.noreply.github.com>
2024-02-09 14:55:51 +0100

    initialization of the spring project

---
+++ OrderSharing/src/main/java/org/example/ordersharing/OrderSharingApplication.java
@@ -0,0 +1,21 @@
+package org.example.ordersharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class OrderSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSharingApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ OrderSharing/src/test/java/org/example/ordersharing/OrderSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ordersharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
