b15d76cc1bd13126df43f34e0e29f07da72fd613
RomainMarques <77971683+RomainMarques@users.noreply.github.com>
2024-02-11 16:58:13 +0100

    Update init.js

--- database/init.js
+++ database/init.js
@@ -1,9 +1,28 @@
-// Create a 'users' collection
+db.createCollection('park');
+db.park.insertMany([
+    { name: 'A', location: 'A' },
+    { name: 'B', location: 'B' },
+    { name: 'C', location: 'C' },
+]);
+
+
 db.createCollection('users');
 
-// Insert sample data into the 'users' collection
 db.users.insertMany([
     { name: 'John', role: 'customer' },
-    { name: 'Alice', role: 'agent' },
-    // Add more sample data as needed
+    { name: 'Alice', role: 'agent', park: 'A' }
 ]);
+
+db.createCollection('products');
+db.products.insertMany([
+    { name: 'Fries', price: 100, park: 'A' },
+    { name: 'Coca Cola', price: 200, park: 'A'},
+    { name: 'Burger', price: 300, park: 'B' },
+]);
+
+db.createCollection('orders');
+db.orders.insertMany([
+    { customer: 'John', park: 'A', product: ['Fries', 'Coca Cola'] },
+    { customer: 'John', park: 'B', product: ['Burger'] },
+]);
+
