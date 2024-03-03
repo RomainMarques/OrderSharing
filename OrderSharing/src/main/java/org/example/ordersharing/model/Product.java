99c13ad848c3aae3f88eb633e680d767f2b37d56
Jathoosh <73128006+Jathoosh@users.noreply.github.com>
2024-03-04 00:31:17 +0100

    Add Products management endpoints

--- OrderSharing/src/main/java/org/example/ordersharing/model/Product.java
+++ OrderSharing/src/main/java/org/example/ordersharing/model/Product.java
@@ -2,6 +2,7 @@ package org.example.ordersharing.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,11 +10,19 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "products")
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Product {
     @Id
     private String id;
     private String name;
-    private int price;
+    private double price;
     private String parkName;
+
+    public Product (String name, double price, String parkName) {
+        this.id = String.valueOf(parkName.concat(name).hashCode());
+        this.name = name;
+        this.price = price;
+        this.parkName = parkName;
+    }
 }
